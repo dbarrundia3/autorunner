2089c087bd8678c42c2114a2275ed204cfa92cc7
dbarrundia3 <dbarrundia3@gatech.edu>
2017-09-14 15:27:57 -0400

    Try/Except clause in case of tools failing

--- tools.py
+++ tools.py
@@ -2,6 +2,11 @@ import inspect
 import ast
 
 def test_call(source,target):
-    call_names = [c.func.id for c in ast.walk(ast.parse(inspect.getsource(source)))
-              if isinstance(c, ast.Call)]
+    call_names = []
+    for c in ast.walk(ast.parse(inspect.getsource(source))):
+        if isinstance(c, ast.Call):
+            try:
+                call_names.append(c.func.id)
+            except Exception as e:
+                pass
     return target.__name__ in call_names
